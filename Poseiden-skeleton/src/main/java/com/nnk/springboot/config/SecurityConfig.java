6d626450411000cc0a7b1ec857bd3f41125f0c50
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-01-05 16:41:54 +0100

    Config Security WIP

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SecurityConfig.java
@@ -1,75 +1,62 @@
-//package com.nnk.springboot.config;
-//
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-//
-//@EnableWebSecurity
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers(
-//                        "/login", "/registry", "/transactions/**", "/process_register", "/resources/**", "/css/**", "/fonts/**", "/img/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login").permitAll()
-//                .and()
-//                .logout().invalidateHttpSession(true)
-//                .clearAuthentication(true)
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                .logoutSuccessUrl("/login").permitAll()
-//                .and()
-//                .rememberMe()
-//                .tokenValiditySeconds(365 * 24 * 60 * 60) // expiration time: 1 an
-//                .key("AbcdefghiJklmNoPqRstUvXyz").userDetailsService(this.userDetailService);
-//
-//    }
-//
-//
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Autowired
-//    private UserDetailsService userDetailService;
-//
-//    @Bean
-//    public AuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-//
-//        provider.setUserDetailsService(userDetailService);
-//        provider.setPasswordEncoder(passwordEncoder());
-//
-//        return provider;
-//    }
-//
-//
-//}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+package com.nnk.springboot.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled=true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private UserDetailsService userDetailService;
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/bidList/**", "/rating/**", "/ruleName/**", "/trade/**", "/curvePoint/**").hasAnyAuthority("ADMIN", "USER")
+                    .antMatchers("/user/**").permitAll()
+                    .and().formLogin()  //login configuration
+                    .defaultSuccessUrl("/bidList/list")
+                    .and().logout()    //logout configuration
+                    .logoutUrl("/app-logout")
+                    .logoutSuccessUrl("/")
+                    .and().exceptionHandling() //exception handling configuration
+                    .accessDeniedPage("/app/error");
+        }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailService);
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
