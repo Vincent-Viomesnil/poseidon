8a48f04dcf598f29d3f5d04f8053b85aa4c6bde9
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-01-06 14:58:23 +0100

    Config Security WIP

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/CurvePointTests.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/CurvePointTests.java
@@ -5,6 +5,7 @@ import com.nnk.springboot.repositories.CurvePointRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class CurvePointTests {
 
 	@Autowired
@@ -20,12 +22,12 @@ public class CurvePointTests {
 
 	@Test
 	public void curvePointTest() {
-		CurvePoint curvePoint = new CurvePoint(10, 10d, 30d);
+		CurvePoint curvePoint = new CurvePoint(17, 10d, 30d);
 
 		// Save
 		curvePointRepository.save(curvePoint);
 		assertNotNull(curvePoint.getId());
-		assertTrue(curvePoint.getCurveId() == 10);
+		assertTrue(curvePoint.getCurveId() == 17);
 
 		// Update
 		curvePoint.setCurveId(20);
