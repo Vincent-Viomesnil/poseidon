052632954097fed15b06e98c506ad5e8784ac410
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-01-07 03:42:22 +0100

    Controllers OK

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RuleNameRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RuleNameRepository.java
@@ -3,6 +3,10 @@ package com.nnk.springboot.repositories;
 import com.nnk.springboot.domain.RuleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface RuleNameRepository extends JpaRepository<RuleName, Integer> {
+
+    Optional<RuleName> findById(Integer id);
 }
